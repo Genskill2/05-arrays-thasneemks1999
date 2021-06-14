cdd3792faddd913f337f2c3589e89280d074c898
thasneemks1999 <85107162+thasneemks1999@users.noreply.github.com>
2021-06-14 22:51:15 +0530

    Update test_average.c

--- test_average.c
+++ test_average.c
@@ -5,10 +5,10 @@
 float average(int [], int);
 
 int main(void) {
-  int x[] = {9,5,6,10,2,-3,4};
-  assert ((average(x, 7) - 4.7142) < 0.001);
+  int y[] = {9,5,6,10,2,-3,4};
+  assert ((average(y, 7) - 4.7142) < 0.001);
 
-  int y[] = {5};
-  assert (average(y, 1) == 5.0);
+  int x[] = {5};
+  assert (average(x, 1) == 5.0);
   printf("Average: passed\n");
   }
