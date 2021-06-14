42e576a087277d8060d1e6f052e3696ca95064b4
thasneemks1999 <85107162+thasneemks1999@users.noreply.github.com>
2021-06-14 22:42:18 +0530

    update solns.c

--- solns.c
+++ solns.c
@@ -1,110 +1,20 @@
 /* Enter your solutions in this file */
 #include<stdio.h>
 
-int main(void){
-
-  /*finding average*/
-
-  float average(int len,int arr[]) {
-
-   float sum = 0;
-   float average;
-
-   for (int c = 0; c<len; c++){
-
-   sum+=arr[c];
-
-   }
-
-   return sum/length;
-
-  }
-
+/*average function*/
+float average(int arr[], int len){
   
-
-  /*finding maximum*/
-
-  int maximum(int len,int arr[]){
-
-   int max_so_far;
-
-  max_so_far = arr[0];
-
-  for (int k=0; k<len; k++){
-
-   if(marks[k] > max_so_far){
-
-   max_so_far = marks[k];
-
-   }
-
-   }
-
-   return max_so_far;
-
-  }
-
+  int sum = 0;
   
-
-  /*finding minimum*/
-
-  int minimum(int length,int arr[]){
-
-   int min_so_far;
-
-  min_so_far = arr[0];
-
-  for (int j=0; j<len; j++){
-
-   if(marks[j] < min_so_far){
-
-   min_so_far = marks[j];
-
-   }
-
-   }
-
-   return min_so_far;
-
+  float avg;
+  for(int c=0; c<len; c++){
+    sum = sum + arr[c];
   }
-/* mode function*/
-int mode(int arr[], int len){
-  
-  int ar2[len];
-  
-  int l,c, max;
-  
-  int cnt = 0;
-  
-  for(int i = 0; i< len; i++){
-    
-  l = arr[i];
-  for(int j=0; j<len; j++){
-  if( arr[j] == l)
-  cnt = cnt +1;
-  }
-    
-  ar2[i] = cnt;
-  cnt = 0;
-  }
-  
-  max = ar2[0];
-  for(int m = 0; m< len; m++){
-  if(ar2[m]> max){
-  max = ar2[m];
-  }
-    
-  }
-  for(f =0; f<len; f++){
-  if(max == ar2[f])
-  break;
-  }
-  
-  return arr[f];
-  }
-  
+  avg = (float)sum/len;
+  return avg;
+}
+
 /*factors function*/
-  
 int factors(int num, int arr[]){
 int cpy = num;
 int cnt = 0;
@@ -115,14 +25,12 @@ for(int j=2; j<=i; j++){
 if(i%j==0)
 cnt+= 1;
 }
-  
 if(cnt ==1){
 while(cpy%i ==0){
 arr[cnt1] = i;
 cpy/= i;
 cnt1+= 1;
 }
-  
 cpy = num;}
 cnt = 0;
 }
@@ -130,3 +38,52 @@ else
 continue;}
 return cnt1;
 }
+
+/* max function*/
+int max(int arr[], int len){
+  int maximum = arr[0];
+  for(int i= 0; i< len; i++){
+    if(arr[i]> maximum)
+      maximum = arr[i];
+  }
+  return maximum;
+       }
+
+/* min function*/
+     int min( int arr[], int len){
+  int min;
+  min = arr[0];
+  for(int k=0; k<len; k++){
+    if(arr[k]<min){
+      min = arr[k];
+    }
+  }
+  return min;
+}
+
+/* mode function*/
+int mode(int arr[], int len){
+  int arr2[len];
+  int l,c, max;
+  int cnt = 0;
+  for(int i = 0; i< len; i++){
+  l = arr[i];
+  for(int j=0; j<len; j++){
+  if( arr[j] == l)
+  cnt = cnt +1;
+  }
+  arr2[i] = cnt;
+  cnt = 0;
+  }
+  max = arr2[0];
+  for(int i = 0; i< len; i++){
+  if(arr2[i]> max){
+  max = arr2[h];
+  }
+  }
+  for(c =0; c<len; c++){
+  if(max == ar2[c])
+  break;
+  }
+  return arr[c];
+  }
