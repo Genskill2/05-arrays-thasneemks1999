24d02a03bab59459c6cfbe7ac3e6e47ba143bf33
thasneemks1999 <85107162+thasneemks1999@users.noreply.github.com>
2021-06-14 22:43:58 +0530

    update solns.c

--- solns.c
+++ solns.c
@@ -3,17 +3,14 @@
 
 /*average function*/
 float average(int arr[], int len){
-  
   int sum = 0;
-  
   float avg;
-  for(int c=0; c<len; c++){
-    sum = sum + arr[c];
+  for(int p=0; p<len; p++){
+    sum = sum + arr[p];
   }
   avg = (float)sum/len;
   return avg;
 }
-
 /*factors function*/
 int factors(int num, int arr[]){
 int cpy = num;
@@ -38,7 +35,6 @@ else
 continue;}
 return cnt1;
 }
-
 /* max function*/
 int max(int arr[], int len){
   int maximum = arr[0];
@@ -48,42 +44,40 @@ int max(int arr[], int len){
   }
   return maximum;
        }
-
 /* min function*/
      int min( int arr[], int len){
   int min;
   min = arr[0];
-  for(int k=0; k<len; k++){
-    if(arr[k]<min){
-      min = arr[k];
+  for(int h=0; h<len; h++){
+    if(arr[h]<min){
+      min = arr[h];
     }
   }
   return min;
 }
-
 /* mode function*/
 int mode(int arr[], int len){
-  int arr2[len];
-  int l,c, max;
+  int ar2[len];
+  int n,p, max;
   int cnt = 0;
   for(int i = 0; i< len; i++){
-  l = arr[i];
+  n = arr[i];
   for(int j=0; j<len; j++){
-  if( arr[j] == l)
+  if( arr[j] == n)
   cnt = cnt +1;
   }
-  arr2[i] = cnt;
+  ar2[i] = cnt;
   cnt = 0;
   }
-  max = arr2[0];
-  for(int i = 0; i< len; i++){
-  if(arr2[i]> max){
-  max = arr2[h];
+  max = ar2[0];
+  for(int h = 0; h< len; h++){
+  if(ar2[h]> max){
+  max = ar2[h];
   }
   }
-  for(c =0; c<len; c++){
-  if(max == ar2[c])
+  for(p =0; p<len; p++){
+  if(max == ar2[p])
   break;
   }
-  return arr[c];
+  return arr[p];
   }
